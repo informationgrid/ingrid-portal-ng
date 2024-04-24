5139661389caafed76487d78909f08b9bfd8e4e2
André Wallat <andre.wallat@wemove.com>
2024-04-24 14:02:17 +0200

    Add composer to development container

--- user/plugins/ingrid-search-result/vendor/composer/autoload_real.php
+++ user/plugins/ingrid-search-result/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitbc684162e668785e8690996788bb8c0e
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitbc684162e668785e8690996788bb8c0e::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
