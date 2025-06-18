96dfc3d8c56e4074387e9b400a009700d94f3aba
Toan Tran <khanh.toan.tran@wemove.com>
2025-06-18 14:49:04 +0200

    UVP: Update search page

--- user/themes/ingrid/js/ingrid.js
+++ user/themes/ingrid/js/ingrid.js
@@ -412,20 +412,22 @@ function checkPassword(pwd, idMeter, idText) {
 }
 
 function updateQueryStringParameter(key, value) {
-  var uri = window.parent.location.href;
-  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-  if (uri.match(re)) {
-    uri = uri.replace(re, '$1' + key + "=" + value + '$2');
-  }
-  else {
-    uri = uri + separator + key + "=" + value;
-  }
-  window.history.replaceState(null,null, uri);
+    var location = window.parent.location ?? window.document.location;
+    var uri = location.href;
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+        uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+    }
+    else {
+        uri = uri + separator + key + "=" + value;
+    }
+    window.history.replaceState(null,null, uri);
 }
 
 function getQueryStringParameter(key) {
-    var url = window.parent.location.href;
+    var location = window.parent.location ?? window.document.location;
+    var url = location.href;
     key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
     var regexS = "[\\?&]"+key+"=([^&#]*)";
     var regex = new RegExp( regexS );
@@ -435,19 +437,19 @@ function getQueryStringParameter(key) {
 }
 
 function updateURLParamReload(key, value) {
-  var pathname = window.parent.location.pathname;
-  var search = window.parent.location.search;
-  var hash = window.parent.location.hash;
-  var uri = pathname + search;
-  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-  if (uri.match(re)) {
-    uri = uri.replace(re, '$1' + key + "=" + value + '$2');
-  }
-  else {
-    uri = uri + separator + key + "=" + value;
-  }
-  window.location.href = uri + hash;
+    var location = window.parent.location ?? window.document.location;
+    var pathname = location.pathname;
+    var search = location.search;
+    var hash = location.hash;
+    var uri = pathname + search;
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+        uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+    } else {
+        uri = uri + separator + key + "=" + value;
+    }
+    window.location.href = uri + hash;
 }
 
 function getOS() {
@@ -530,4 +532,103 @@ function loadDefaultMapImage(elem, partner, theme) {
         }
       }
     }
-  }
+}
+
+function cloneOptions (options) {
+    var ret = {};
+    for (var i in options) {
+        var item = options[i];
+        if (item && item.clone) {
+            ret[i] = item.clone();
+        } else if (item instanceof L.Layer) {
+            ret[i] = cloneLayer(item);
+        } else {
+            ret[i] = item;
+        }
+    }
+    return ret;
+}
+
+function cloneInnerLayers (layer) {
+    var layers = [];
+    layer.eachLayer(function (inner) {
+        layers.push(cloneLayer(inner));
+    });
+    return layers;
+}
+
+function cloneLayer (layer) {
+    var options = cloneOptions(layer.options);
+
+    // we need to test for the most specific class first, i.e.
+    // Circle before CircleMarker
+
+    // Renderers
+    if (layer instanceof L.SVG) {
+        return L.svg(options);
+    }
+    if (layer instanceof L.Canvas) {
+        return L.canvas(options);
+    }
+
+    // GoogleMutant GridLayer
+    if (L.GridLayer.GoogleMutant && layer instanceof L.GridLayer.GoogleMutant) {
+        var googleLayer = L.gridLayer.googleMutant(options);
+
+        layer._GAPIPromise.then(function () {
+            var subLayers = Object.keys(layer._subLayers);
+
+            for (var i in subLayers) {
+                googleLayer.addGoogleLayer(subLayers[i]);
+            }
+        });
+
+        return googleLayer;
+    }
+
+    // Tile layers
+    if (layer instanceof L.TileLayer.WMS) {
+        return L.tileLayer.wms(layer._url, options);
+    }
+    if (layer instanceof L.TileLayer) {
+        return L.tileLayer(layer._url, options);
+    }
+    if (layer instanceof L.ImageOverlay) {
+        return L.imageOverlay(layer._url, layer._bounds, options);
+    }
+
+    // Marker layers
+    if (layer instanceof L.Marker) {
+        return L.marker(layer.getLatLng(), options);
+    }
+
+    if (layer instanceof L.Circle) {
+        return L.circle(layer.getLatLng(), layer.getRadius(), options);
+    }
+    if (layer instanceof L.CircleMarker) {
+        return L.circleMarker(layer.getLatLng(), options);
+    }
+
+    if (layer instanceof L.Rectangle) {
+        return L.rectangle(layer.getBounds(), options);
+    }
+    if (layer instanceof L.Polygon) {
+        return L.polygon(layer.getLatLngs(), options);
+    }
+    if (layer instanceof L.Polyline) {
+        return L.polyline(layer.getLatLngs(), options);
+    }
+
+    if (layer instanceof L.GeoJSON) {
+        return L.geoJson(layer.toGeoJSON(), options);
+    }
+
+    if (layer instanceof L.FeatureGroup) {
+        return L.markerClusterGroup(options);
+    }
+    if (layer instanceof L.LayerGroup) {
+        return L.layerGroup(cloneInnerLayers(layer));
+    }
+
+    throw 'Unknown layer, cannot clone this layer. Leaflet-version: ' + L.version;
+}
